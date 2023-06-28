478b13a9d71791719c41bc97f9daa148de7d6857
irwinekabakti <irwinekabakti@gmail.com>
2023-06-28 17:26:29 +0700

    fixed:(API Heroku )

--- src/api/BASE_API.js
+++ src/api/BASE_API.js
@@ -1,3 +1,5 @@
-const BASE_API = `https://api-car-rental.binaracademy.org`;
+// const BASE_API = `https://api-car-rental.binaracademy.org`;
+
+const BASE_API = `https://bootcamp-rent-cars.herokuapp.com`;
 
 export default BASE_API;
